8035637e0043ee00ca6cc4391065c203d5e0e303
oliyka <olja.x.06@gmail.com>
2025-07-07 01:43:10 +0300

    added img

--- src/js/events.js
+++ src/js/events.js
@@ -1,79 +1,50 @@
-// core version + navigation, pagination modules:
 import Swiper from 'swiper';
-import { Navigation, Pagination } from 'swiper/modules';
-// import Swiper and modules styles
+import { Navigation } from 'swiper/modules';
 import 'swiper/css';
-// import 'swiper/css/navigation';
-// import 'swiper/css/pagination';
+import 'swiper/css/navigation';
 
-
-// const swiper = new Swiper('.swiper', {
-
-//   direction: 'vertical',
-//   loop: true,
-
-//   pagination: {
-//     el: '.swiper-pagination',
-//   },
-
-//   navigation: {
-//     nextEl: '.swiper-button-next',
-//     prevEl: '.swiper-button-prev',
-//   },
-
-
-// });
+const prevBtn = document.querySelector('.swiper-button-prev');
+const nextBtn = document.querySelector('.swiper-button-next');
+let swiperInstance = null;
 
 const swiper = new Swiper('.swiper', {
+  modules: [Navigation],
   slidesPerView: 1,
-  loop: true,
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-  },
+  loop: false,
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
+  },
+  on: {
+    init: updateButtonsState,
+    slideChange: updateButtonsState,
   },
   breakpoints: {
     768: {
       slidesPerView: 2,
     },
-    1024: {
+    1440: {
       slidesPerView: 3,
     }
-  }
+  },
 });
 
-let swiperInstance = null;
 
-function initSwiper() {
-  const screenWidth = window.innerWidth;
-
-  // Якщо ширина менше 1024 і Swiper ще не створений
-  if (screenWidth < 1024 && !swiperInstance) {
-    swiperInstance = new Swiper('.swiper', {
-      slidesPerView: 1,
-      loop: true,
-      pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-      },
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-      breakpoints: {
-        768: {
-          slidesPerView: 2,
-        }
-      }
-    });
+function updateButtonsState(swiper) {
+  if (swiper.isBeginning) {
+    prevBtn.disabled = true;
+    prevBtn.classList.add('disabled');
+  } else {
+    prevBtn.disabled = false;
+    prevBtn.classList.remove('disabled');
   }
 
-  // Якщо ширина >= 1024 і Swiper вже існує — знищити
-  if (screenWidth >= 1024 && swiperInstance) {
-    swiperInstance.destroy(true, true);
-    swiperInstance = null;
+  
+  if (swiper.isEnd) {
+    nextBtn.disabled = true;
+    nextBtn.classList.add('disabled');
+  } else {
+    nextBtn.disabled = false;
+    nextBtn.classList.remove('disabled');
   }
 }
