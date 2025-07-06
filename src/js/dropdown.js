156f98976e42b006d457334e7daca903cda31d9e
LyubovNesterchuk <nesterchuklyubov@gmail.com>
2025-07-06 23:22:13 +0300

    fix category styles

--- src/js/dropdown.js
+++ src/js/dropdown.js
@@ -1,20 +1,7 @@
-// const toggle = document.querySelector('.dropdown-toggle');
-// const dropdown = document.querySelector('.dropdown');
-
-// toggle.addEventListener('click', () => {
-//   dropdown.classList.toggle('open');
-// });
-
-// document.addEventListener('click', (event) => {
-//   if (!dropdown.contains(event.target)) {
-//     dropdown.classList.remove('open');
-//   }
-// });
-
 const toggle = document.querySelector('.dropdown-toggle');
 const dropdown = document.querySelector('.dropdown');
 const links = document.querySelectorAll('.dropdown-menu a');
-
+const label = document.querySelector('.dropdown-label');
 
 toggle.addEventListener('click', (event) => {
   event.stopPropagation();
@@ -27,14 +14,12 @@ document.addEventListener('click', (event) => {
   }
 });
 
-
 links.forEach(link => {
   link.addEventListener('click', (event) => {
     event.preventDefault();
 
-    const selectedCategory = link.dataset.category || link.textContent;
-    
-    toggle.childNodes[0].textContent = selectedCategory;
+    const selectedCategory = link.dataset.category || link.textContent; 
+    label.textContent = selectedCategory;
 
     dropdown.classList.remove('open');
   });
