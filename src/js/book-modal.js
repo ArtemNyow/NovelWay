b3666790b09a0bc3f958faadf5fe06cd87567a13
Yana Khovbosha <aemec4373@gmail.com>
2025-07-06 21:42:56 +0300

    add button functions add to cart and buy now

--- src/js/book-modal.js
+++ src/js/book-modal.js
@@ -1,6 +1,6 @@
 import Accordion from 'accordion-js';
 import 'accordion-js/dist/accordion.min.css';
-import axios from 'axios';
+// import axios from 'axios';
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 
@@ -9,6 +9,9 @@ const closeBtn = document.querySelector('.btn-icon-close');
 const minus = document.querySelector('.minus');
 const inputSum = document.querySelector('.form-input-sum');
 const plus = document.querySelector('.plus');
+const formBookModal = document.querySelector('.form-book-modal');
+const addToCartBtn = document.querySelector('.add-to-cart');
+const buyNowBtn = document.querySelector('.buy-now');
 
 inputSum.value = 1;
 
@@ -43,4 +46,24 @@ plus.addEventListener('click', plusBtn);
 
 function plusBtn(event) {
   inputSum.value = Number(inputSum.value) + 1;
+}
+
+addToCartBtn.addEventListener('click', addToCart);
+
+function addToCart() {
+  const value = inputSum.value.trim();
+  iziToast.success({
+    message: `Кількість обраних товарів: ${value}`,
+    position: 'topRight',
+  });
+}
+
+formBookModal.addEventListener('submit', buyNow);
+
+function buyNow() {
+  event.preventDefault();
+  iziToast.success({
+    message: 'Дякуємо за покупку',
+    position: 'topRight',
+  });
 }
