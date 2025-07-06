6887c2f0b4417b8494d65de320bd3ad20ee1e9c8
Artem <tem4ik20.1@gmail.com>
2025-07-06 13:39:57 +0300

    fix error messege in input

--- src/js/footer.js
+++ src/js/footer.js
@@ -10,18 +10,18 @@ document.addEventListener('DOMContentLoaded', () => {
     const value = emailInput.value.trim();
 
     if (value === '' || !emailInput.checkValidity()) {
-      errorText.hidden = false;             
+      errorText.classList.add('visible');
       emailInput.classList.add('input-error');
     } else {
-      errorText.hidden = true;              
+      errorText.classList.remove('visible');
       emailInput.classList.remove('input-error');
-      form.submit();                        
+      form.submit();
     }
   });
 
   emailInput.addEventListener('input', () => {
     if (emailInput.value.trim() !== '' && emailInput.checkValidity()) {
-      errorText.hidden = true;
+      errorText.classList.remove('visible');
       emailInput.classList.remove('input-error');
     }
   });
