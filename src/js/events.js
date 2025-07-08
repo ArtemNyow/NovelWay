abbfa15c8c7040b2ff20f3afd73c0235669efad4
Artem <tem4ik20.1@gmail.com>
2025-07-08 17:00:24 +0300

    fix size

--- src/js/events.js
+++ src/js/events.js
@@ -3,34 +3,39 @@ import { Navigation } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/navigation';
 
-const prevBtn = document.querySelector('.swiper-button-prev');
-const nextBtn = document.querySelector('.swiper-button-next');
-let swiperInstance = null;
-
-const swiper = new Swiper('.swiper', {
+const swiper = new Swiper('.events-swiper', {
   modules: [Navigation],
   slidesPerView: 1,
   loop: false,
+  roundLengths: true,
+
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    nextEl: '.events-swiper-button-next',
+    prevEl: '.events-swiper-button-prev',
+  },
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 24,
+    },
+    1440: {
+      slidesPerView: 3,
+      spaceBetween: 24,
+      allowSlideNext:false
+    },
   },
   on: {
     init: updateButtonsState,
     slideChange: updateButtonsState,
   },
-  breakpoints: {
-    768: {
-      slidesPerView: 2,
-    },
-    1440: {
-      slidesPerView: 3,
-    }
-  },
 });
 
-
 function updateButtonsState(swiper) {
+  const prevBtn = document.querySelector('.events-swiper-button-prev');
+  const nextBtn = document.querySelector('.events-swiper-button-next');
+
+  if (!prevBtn || !nextBtn) return;
+
   if (swiper.isBeginning) {
     prevBtn.disabled = true;
     prevBtn.classList.add('disabled');
@@ -39,7 +44,6 @@ function updateButtonsState(swiper) {
     prevBtn.classList.remove('disabled');
   }
 
-  
   if (swiper.isEnd) {
     nextBtn.disabled = true;
     nextBtn.classList.add('disabled');
